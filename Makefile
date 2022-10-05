4bb2ddbbeed79c2784c7c1efa7b7915aed078e0b
vinicius.franca <viniciusfrancas019@gmail.com>
2022-10-04 22:48:05 -0300

    set final changes to Server class and index

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 serve:
-	@docker-compose -f "docker-compose.yml" up
+	@sudo docker-compose -f "docker-compose.yml" up
 
 build:
-	@docker-compose -f "docker-compose.yml" up --build
+	@sudo docker-compose -f "docker-compose.yml" up --build
 
 shell:
 	@docker exec -it boilerplate bash
 
 dev:
-	@docker-compose -f "docker-compose-dev.yml" up --build
+	@sudo docker-compose -f "docker-compose-dev.yml" up --build
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,8 @@
-import express, { Application, Request, Response } from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
+import { Server } from "./Server";
+import * as dotenv from 'dotenv';
 
-
-const app: Application = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({ origin: true }))
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Healthy')
-})
+dotenv.config()
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on PORT ${PORT}`)
-})
+(new Server(PORT)).init();
